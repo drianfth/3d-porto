af8b20bafde7c6987d90dad64ec43a2ad5449b30
drianfth <andrianfathulloh05@gmail.com>
2023-04-05 15:40:07 +0700

    first init

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import "./App.css";
+import { Canvas } from "@react-three/fiber";
+import Experience from "./components/Experience";
+
+function App() {
+  return (
+    <Canvas
+      camera={{
+        fov: 64,
+        position: [2.3, 1.5, 2.3],
+      }}
+    >
+      <Experience />
+    </Canvas>
+  );
+}
+
+export default App;
